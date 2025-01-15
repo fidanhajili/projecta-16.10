85f7b822c9766da31662e8019599879d604ae485
fidanhajili <fidanhajili7@gmail.com>
2025-01-15 14:01:00 +0400

    15.01

--- index.js
+++ index.js
@@ -10,7 +10,6 @@
     document.querySelector('.curtain-menu').classList.remove('aktiv')
  })
  
-
  const right1 = document.querySelector('.right1')
  const bizx = document.querySelector('.biz-x-mark')
  const right2 = document.querySelector('.right2')
@@ -51,6 +50,28 @@
                document.querySelector('.partners').classList.remove('aktiv')
             })
 
+            const swiper = new Swiper(".mySwiper", {
+                slidesPerView: "auto",
+                spaceBetween: 30,
+                autoplay: {
+                    delay: 2500,
+                    disableOnInteraction: false,
+                  },
+                breakpoints: {
+                    320: {
+                      slidesPerView: 2,
+                      spaceBetween: 20,
+                    },
+                    768: {
+                      slidesPerView: 4,
+                      spaceBetween: 40,
+                    },
+                    1024: {
+                      slidesPerView: 5,
+                      spaceBetween: 50,
+                    },
+                  },
+              });
 
  const counters = document.querySelectorAll(".counter");
 
